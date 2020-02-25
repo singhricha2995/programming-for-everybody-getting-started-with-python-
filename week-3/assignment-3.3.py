8e3a79eb4d69a5c51641534cc3f202afb8f6a7be
singhricha2995 <61435466+singhricha2995@users.noreply.github.com>
2020-02-25 09:47:31 -0500

    Create assignment-3.3.py

---
+++ week-3/assignment-3.3.py
@@ -0,0 +1,16 @@
+score=input('Enter the score:')
+s=float(score)
+try:
+    if s>=0.9 and s<=1.0:
+        print('A')
+    elif s>=0.8 and s<0.9:
+        print('B')
+    elif s>=0.7 and s<0.8:
+        print('C')
+    elif s>=0.6 and s<0.7:
+        print('D')
+    elif s<0.6:
+        print('F')
+except:
+    print('No score')
+            
